43cbc3e7e7903d75563e89e5ba0cfe1fc2daaa1c
Madhava Carrillo <madhava.carrillo@glovoapp.com>
2021-11-29 00:18:07 +0100

    Proper role foriot  ts

--- terraform/iot-ts-rule.tf
+++ terraform/iot-ts-rule.tf
@@ -4,20 +4,42 @@
 //  sql         = "SELECT temperature, status FROM 'topic/test'"
 //  sql_version = "2016-03-23"
 //
-//  sns {
-//    message_format = "RAW"
-//    role_arn       = aws_iam_role.role.arn
-//    target_arn     = aws_sns_topic.mytopic.arn
-//  }
-//
-//  error_action {
-//    sns {
-//      message_format = "RAW"
-//      role_arn       = aws_iam_role.role.arn
-//      target_arn     = aws_sns_topic.myerrortopic.arn
-//    }
-//  }
+// Waiting for feature https://github.com/hashicorp/terraform-provider-aws/issues/19904
 //}
+
+data "aws_iam_policy_document" "iot_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["iot.amazonaws.com"]
+    }
+  }
+}
+
+
+resource "aws_iam_role" "ts-role-for-iot" {
+  name               = "iot_ts_role"
+  assume_role_policy = data.aws_iam_policy_document.iot_assume_role_policy.json # (not shown)
+
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["timestream:WriteRecords","timestream:DescribeEndpoints"]
+          Effect   = "Allow"
+          Resource = "*"
+        }
+      ]
+    })
+  }
+}
+
+
 //{
 //  "temperature": 18.125,
 //  "status": 0,
