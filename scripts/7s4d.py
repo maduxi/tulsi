1c0259cacdbb19931a729733e68592292786bde3
Madhava Carrillo <madhava.carrillo@glovoapp.com>
2021-11-29 23:11:39 +0100

    7s4d cleanup

--- scripts/7s4d.py
+++ scripts/7s4d.py
@@ -39,22 +39,22 @@ num = {' ': (0, 0, 0, 0, 0, 0, 0),
        '8': (1, 1, 1, 1, 1, 1, 1),
        '9': (1, 1, 1, 1, 0, 1, 1)}
 
-# try:
-while True:
-    n = time.ctime()[11:13] + time.ctime()[14:16]
-    s = str(n).rjust(4)[::-1]
-    # print(s)
-    for digit in range(4):
-        for loop in range(0, 7):
-            GPIO.output(segments[loop], num[s[digit]][loop])
-            # if (int(time.ctime()[18:19])%2 == 0) and (digit == 1):
-            #     GPIO.output(dp, 1)
-            # else:
-            #     GPIO.output(dp, 0)
-        GPIO.output(digits[digit], 0)
-        time.sleep(0.001)
-        GPIO.output(digits[digit], 1)
-# finally:
-#     GPIO.cleanup()
+try:
+    while True:
+        n = time.ctime()[11:13] + time.ctime()[14:16]
+        s = str(n).rjust(4)[::-1]
+        # print(s)
+        for digit in range(4):
+            for loop in range(0, 7):
+                GPIO.output(segments[loop], num[s[digit]][loop])
+                # if (int(time.ctime()[18:19])%2 == 0) and (digit == 1):
+                #     GPIO.output(dp, 1)
+                # else:
+                #     GPIO.output(dp, 0)
+            GPIO.output(digits[digit], 0)
+            time.sleep(0.001)
+            GPIO.output(digits[digit], 1)
+finally:
+    GPIO.cleanup()
 
 
